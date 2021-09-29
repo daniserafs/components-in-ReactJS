fb5ab50776cc3e7463453c57969ac1fed8187403
Daniela Serafim <danyelaserafim@gmail.com>
2021-09-29 16:42:09 -0300

    finish challenge

---
+++ README.md
@@ -0,0 +1,6 @@
+### Chanllange Components in ReactJS
+    -another easy challenge that I needed help on doing. I guess someday I will be able to do it by myself
+
+
+### Transforming parts of the application into components
+    - at first SideBar and Content were inside App.tsx and I was able to turn them into independent components.
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -60,36 +60,15 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      < SideBar 
+      genres={genres}
+      selectedGenreId={selectedGenreId}
+      buttonClickCallback={handleClickButton} 
+      />
+      <Content 
+        selectedGenre={selectedGenre}
+        movies={movies} 
+      />
     </div>
   )
 }
